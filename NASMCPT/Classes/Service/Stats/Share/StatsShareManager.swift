385040e32c56115913403dd0567f603f5236f698
akonst17 <akonst17@gmail.com>
2022-04-14 16:32:21 +0300

    upgrade; build 10

--- NASMCPT/Classes/Service/Stats/Share/StatsShareManager.swift
+++ NASMCPT/Classes/Service/Stats/Share/StatsShareManager.swift
@@ -16,7 +16,7 @@ final class StatsShareManager {
 // MARK: Public
 extension StatsShareManager {
     func sharedContainerURL() -> URL? {
-        FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: "group.nasmcpt.widgets")
+        FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: "group.nasmcpt.widget")
     }
     
     @discardableResult
