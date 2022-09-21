1b8fe7a02a878799133ff9ce6bfbd87d48588ce8
akonst17 <akonst17@gmail.com>
2022-09-21 18:40:26 +0300

    интеграция sdk 2.0; новые урлы; build 16

--- NASMCPT/Classes/Misc/GlobalDefinitions.swift
+++ NASMCPT/Classes/Misc/GlobalDefinitions.swift
@@ -9,18 +9,21 @@ final class GlobalDefinitions {
     static let domainUrl = "https://exams3.mastrapi.com"
     static let apiKey = "q0zoS2BUwB2T4Cjx"
 
-    static let sdkDomainUrl = "https://analytics.korrekted.com"
-    static let sdkApiKey = "X73L3dAyGLnLbZ7X"
-
     static let applicationTag = "EXAM_NASM_CPT"
 
     static let amplitudeApiKey = "b503251969f4b1d7901d2f7d1388d476"
     static let appsFlyerApiKey = "DCciCfYXjMQ8QnkdCg8qzk"
+    
+    static let sdkDomainUrl = "https://analytics.mastrapi.com"
+    static let sdkApiKey = "X73L3dAyGLnLbZ7X"
+    
+    static let otterScaleHost = "https://analytics2.mastrapi.com"
+    static let otterScaleApiKey = "JQT7grSFgE7bO0Eb"
 
     static let appStoreUrl = "https://apps.apple.com/app/nasm-cpt-fitness-prep/id1580266583"
     static let appleAppID = "1580266583"
 
-    static let termsOfServiceUrl = "https://quantresear.ch/terms.html"
-    static let privacyPolicyUrl = "https://quantresear.ch/policy.html"
-    static let contactUsUrl = "https://quantresear.ch/contact.html"
+    static let termsOfServiceUrl = "https://mastrapi.com/terms"
+    static let privacyPolicyUrl = "https://mastrapi.com/policy"
+    static let contactUsUrl = "https://mastrapi.com/contact"
 }
